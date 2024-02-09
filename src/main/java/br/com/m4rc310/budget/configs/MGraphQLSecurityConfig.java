ea5ebc274f608cafc1a918cfd5254075459f387b
Marcelo Lopes <marcelo@MacBook-Pro-de-Marcelo.local>
2024-02-08 22:59:15 -0300

    Security

--- src/main/java/br/com/m4rc310/budget/configs/MGraphQLSecurityConfig.java
+++ src/main/java/br/com/m4rc310/budget/configs/MGraphQLSecurityConfig.java
@@ -1,19 +1,30 @@
 package br.com.m4rc310.budget.configs;
 
-import org.jfree.util.Log;
+import java.util.Objects;
+
 import org.springframework.context.annotation.Configuration;
 
 import br.com.m4rc310.gql.dto.MAuthToken;
-import br.com.m4rc310.gql.dto.MEnumToken;
 import br.com.m4rc310.gql.dto.MUser;
-import br.com.m4rc310.gql.jwt.MGraphQLJwtService;
 import br.com.m4rc310.gql.security.IMAuthUserProvider;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 public class MGraphQLSecurityConfig implements IMAuthUserProvider{
 
 	@Override
 	public MUser authUser(String username, Object password) throws Exception {
+		
+		if ("test".equals(username)) {
+			MUser user = new MUser();
+			user.setCode(1L);
+			user.setUsername(username);
+			user.setPassword(password + "");
+			user.setRoles("Admin".split(";"));
+			return user;
+		}
+		
 		return null;
 	}
 
@@ -21,47 +32,50 @@ public class MGraphQLSecurityConfig implements IMAuthUserProvider{
 	public MUser getUserFromUsername(String username) {
 		return null;
 	}
-
-	@Override
-	public MUser loadUser(MGraphQLJwtService jwt, MEnumToken type, String token) throws Exception {
-		
-		MUser u = null ;
-		
-		switch (type) {
-		case TEST:
-			Log.info(token);
-			int i = token.indexOf(":");
-			String username = token.substring(0, i);
-			String password = token.substring(i+1);
-			
-			
-			if ("text".equalsIgnoreCase(username)) {
-				u = new MUser();
-				u.setUsername(username);
-				u.setPassword(password);
-				u.setRoles("Admin".split(";"));
-				return u;
-			}else {
-				throw new Exception("Invalid credentials");
-			}
-		case BASIC:
-		case BEARER:
-			return u;
-		default:
-			return u;
-		}
-		
-		
-//		MUser u = new MUser();
-//		u.setUsername("test");
-//		u.setPassword("test");
-//		u.setRoles("Admin".split(";"));
+ 
+//	@Override
+//	public MUser loadUser(MGraphQLJwtService jwt, MEnumToken type, String token) throws Exception {
 //		
-//		return u;
-	}
+//		log.info("{}", type );
+//		MUser u = null ;
+//		
+//		switch (type) {
+//		case TEST:
+//			int i = token.indexOf(":");
+//			String username = token.substring(0, i);
+//			String password = token.substring(i+1);
+//			
+//			log.info("{} -> {}", username, password);
+//			
+//			if ("test".equalsIgnoreCase(username)) {
+//				u = new MUser();
+//				u.setUsername(username);
+//				u.setPassword(password);
+//				u.setRoles("Admin".split(";"));
+//				return u;
+//			}else {
+//				throw new Exception("Invalid credentials");
+//			}
+//		case BASIC:
+//		case BEARER:
+//		default:
+//			throw new Exception("Invalid credentials");
+//		}
+//		
+//		
+////		MUser u = new MUser();
+////		u.setUsername("test");
+////		u.setPassword("test");
+////		u.setRoles("Admin".split(";"));
+////		
+////		return u;
+//	}
 
 	@Override
 	public boolean isValidUser(MUser user) {
+		if (Objects.isNull(user)) {
+			return false;
+		}
 		return true;
 	}
 
--- src/main/java/br/com/m4rc310/budget/services/MServiceTest.java
+++ src/main/java/br/com/m4rc310/budget/services/MServiceTest.java
@@ -5,13 +5,16 @@ import org.springframework.stereotype.Service;
 
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @GraphQLApi
 public class MServiceTest extends MService{
 
 	@GraphQLQuery(name = QUERY$test, description = DESC$query_test)
 	public String queryTest() {
+		log.info("#query test");
 		return "OK";
 	}
 }
